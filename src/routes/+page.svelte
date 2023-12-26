fc86aeb2d8935ab12b3d9397f630941885943677
Kenni Kollemorten <kenn7575@gmail.com>
2023-12-26 15:29:29 +0100

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { sql } from '@vercel/postgres';
+
+	async function getData() {
+		const { rows } = await sql`SELECT * FROM posts WHERE likes`;
+	}
+</script>
+
+this is the book store
